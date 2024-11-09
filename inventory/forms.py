49d1f280ea1579aef95e585cde8e648145e5a8b0
Muli Job <munyoki912@gmail.com>
2024-11-10 01:50:43 +0300

    Added form validation in adding items form

--- inventory/forms.py
+++ inventory/forms.py
@@ -6,6 +6,24 @@ class StockCreateForm(forms.ModelForm):
     model = Stock
     fields = ['category', 'item_name', 'quantity']
 
+  def clean_category(self):
+    category = self.cleaned_data.get('category')
+    if not category:
+      raise forms.ValidationError('This field is required')
+    for instance in Stock.objects.all():
+      if instance.category == category:
+        raise forms.ValidationError(category + ' is already created')
+    return category
+  
+  def clean_item_name(self):
+    item_name = self.cleaned_data.get('item_name')
+    if not item_name:
+      raise forms.ValidationError('This field is required')
+    for instance in Stock.objects.all():
+      if instance.item_name == item_name:
+        raise forms.ValidationError(item_name + ' is already created')
+    return item_name
+
 class StockSearchForm(forms.ModelForm):
   class Meta:
     model = Stock

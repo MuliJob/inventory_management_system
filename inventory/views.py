3b0f3e26e1b92d442d0f9381ae30fef57c078053
Muli Job <munyoki912@gmail.com>
2024-11-10 17:45:36 +0300

    exporting records to csv

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Stock
 from .forms import StockCreateForm, StockSearchForm, StockUpdateForm
+from django.http import HttpResponse
+import csv
 
 # Create your views here.
 def home(request):
@@ -23,6 +25,15 @@ def list_items(request):
     queryset = Stock.objects.filter(category__icontains=form['category'].value(),
                                     item_name__icontains=form['item_name'].value()
                                     )
+    if form['export_to_CSV'].value() == True:
+      response = HttpResponse(content_type='text/csv')
+      response['Content-Disposition'] = 'attachment; filename="List of stock.csv"'
+      writer = csv.writer(response)
+      writer.writerow(['CATEGORY', 'ITEM NAME', 'QUANTITY'])
+      instance = queryset
+      for stock in instance:
+        writer.writerow([stock.category, stock.item_name, stock.quantity])
+      return response
     context = {
       'form': form,
       'header': header,

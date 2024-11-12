ef63fc9850e3fd224974addbdecd35d2da6ef211
Muli Job <munyoki912@gmail.com>
2024-11-12 23:13:51 +0300

    Added functionality for issue and receive items and anchor links to navigate to the issue and receive form

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Stock
-from .forms import StockCreateForm, StockSearchForm, StockUpdateForm
+from .models import *
+from .forms import *
 from django.http import HttpResponse
 import csv
 from django.contrib import messages
@@ -83,3 +83,45 @@ def stock_detail(request, pk):
     'queryset': queryset,
   }
   return render(request, 'stock-detail.html', context)
+
+def issue_items(request, pk):
+  queryset = Stock.objects.get(id=pk)
+  form = IssueForm(request.POST or None, instance=queryset)
+  if form.is_valid():
+    instance = form.save(commit=False)
+    instance.quantity -= instance.issue_quantity
+    # instance.issue_by = str(request.user)
+    messages.success(request, 'Issued SUCCESSFULLY. ' + str(instance.quantity) + ' ' + str(instance.item_name) + 's now left in Store')
+    instance.save()
+
+    return redirect('/stock-detail/'+str(instance.id))
+    # return HttpResponseRedirect(instance.get_absolute_url())
+  
+  context = {
+    'title': 'Issue ' + str(queryset.item_name),
+    'queryset': queryset,
+    'form': form,
+    'username': 'Issue By: ' + str(request.user),
+  }
+  return render(request, 'add-items.html', context)
+
+def receive_items(request, pk):
+  queryset = Stock.objects.get(id=pk)
+  form = ReceiveForm(request.POST or None, instance=queryset)
+  if form.is_valid():
+    instance = form.save(commit=False)
+    instance.quantity += instance.receive_quantity
+    instance.save()
+    messages.success(request, 'Received SUCCESSFULLY. ' + str(instance.quantity) + ' ' + str(instance.item_name) + 's now in Store')
+
+
+    return redirect('/stock-detail/'+str(instance.id))
+    # return HttpResponseRedirect(instance.get_absolute_url())
+  
+  context = {
+    'title': 'Receive ' + str(queryset.item_name),
+    'instance': queryset,
+    'form': form,
+    'username': 'Received By: ' + str(request.user),
+  }
+  return render(request, 'add-items.html', context)

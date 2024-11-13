4daaa8d864a78b9ad9d43723173bebb991f81b54
Muli Job <munyoki912@gmail.com>
2024-11-13 03:06:05 +0300

    removed date form field

--- inventory/forms.py
+++ inventory/forms.py
@@ -4,7 +4,7 @@ from .models import Stock
 class StockCreateForm(forms.ModelForm):
   class Meta:
     model = Stock
-    fields = ['category', 'item_name', 'quantity', 'date']
+    fields = ['category', 'item_name', 'quantity']
 
   def clean_category(self):
     category = self.cleaned_data.get('category')

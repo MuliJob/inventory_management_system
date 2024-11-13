0e1e2599c1ec6ff1f2b9d1d40aae7a0531429503
Muli Job <munyoki912@gmail.com>
2024-11-13 03:06:35 +0300

    removed date in stock model

--- inventory/models.py
+++ inventory/models.py
@@ -27,7 +27,7 @@ class Stock(models.Model):
   reorder_level = models.IntegerField(default='0', blank=True, null=True)
   last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
   timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-  date = models.DateTimeField(auto_now_add=False, auto_now=False)
+  # date = models.DateTimeField(auto_now_add=False, auto_now=False)
   export_to_CSV = models.BooleanField(default=False)
 
   def __str__(self):

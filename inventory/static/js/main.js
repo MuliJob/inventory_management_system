b8792d49cc43797c15a363375b2effd2c46570f4
Muli Job <munyoki912@gmail.com>
2024-11-15 14:24:23 +0300

    Adding pagination and progress bar

--- inventory/static/js/main.js
+++ inventory/static/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+  $('.myPages').pxpaginate({
+    limit: 6,
+    callback:function(pagenumber){
+          console.log(pagenumber);
+        }
+      
+  });
+
+  NProgress.start();
+  NProgress.done();
+
   $('.datetimeinput').datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
 })
